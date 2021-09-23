339240a4d28be8e34f4afdc973695e26d383211c
Jimster280 <razumov_feodor@mail.ru>
2021-09-23 11:32:59 +0300

    create: review section and fix hotel slider

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-const swiper = new Swiper('.swiper', {
+const hotelSlider = new Swiper('.hotel-swiper', {
   // Optional parameters
   loop: true,
   // клавиатура
@@ -7,12 +7,26 @@ const swiper = new Swiper('.swiper', {
   },
   // Navigation arrows
   navigation: {
-    nextEl: '.slider-button--next',
-    prevEl: '.slider-button--prev',
+    nextEl: '.hotel-slider-button--next',
+    prevEl: '.hotel-slider-button--prev',
   },
 });
 
 
 $(function(){
   $('.newsletter').parallax({imageSrc: '../img/newsletterbackground.jpg'});
+});
+
+const reviewsSlider = new Swiper('.reviews-slider', {
+  // Optional parameters
+  loop: true,
+  // клавиатура
+  keyboard: {
+    enabled: true,
+  },
+  // Navigation arrows
+  navigation: {
+    nextEl: '.reviews-slider__button--next',
+    prevEl: '.reviews-slider__button--prev',
+  },
 });
